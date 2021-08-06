cb7aa7318a2ac542e76eebed524a53a40c30b064
Seuntjie900 <seuntjie900@gmail.com>
2021-08-06 11:25:10 +0200

    Switching PCs, making sure everything is pushed. Probably breaks a lot of things.

---
+++ KryGamesBotAva/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KryGamesBotAva.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
