5d92b1c31b740ff87d1ea66e6cc8e1b6b7228615
Eugene <Seuntjie900@users.noreply.github.com>
2023-10-03 01:02:54 +0200

    finished converting strat functions, haven't started with layouts yet

--- KryGamesBot.MAUI.Blazor/Classes/SimpleDataPoint.cs
+++ KryGamesBot.MAUI.Blazor/Classes/SimpleDataPoint.cs
@@ -4,12 +4,16 @@
     {
 
         public double Argument { get; private set; }
-        public double Value { get; private set; }
-
-        public SimpleDataPoint(double arg, double val)
+        public double? Value { get; private set; }
+        public string Color { get; set; }
+        public SimpleDataPoint(double arg, double? val)
         {
             Argument = arg;
             Value = val;
+        }
+        public SimpleDataPoint(double arg, double? val,string color):this(arg,val)
+        {
+            Color = color;
         }
     }
 }
